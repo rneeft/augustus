ddc41dc09e4c69823f0c0a7ccdcf117d86d95a26
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2025-08-29 14:14:08 +0300

    Fix tooltip text of ratings in the top menu panel (#1450)
    Fix tooltip text of ratings in the top menu panel
    
    Increase the health rating offset from the main ratings

--- src/widget/top_menu.c
+++ src/widget/top_menu.c
@@ -543,7 +543,7 @@ static int draw_health_panel(int offset, int box_width, font_t font)
 
     // center it in the box
     int x = offset + (box_width - health_w) / 2;
-    text_draw_number(health, ' ', "", x, 5, font, color);
+    text_draw_number(health, ' ', "", x + 10, 5, font, color);
 
     return box_width;
 }
@@ -867,20 +867,19 @@ int widget_top_menu_get_tooltip_text(tooltip_context *c)
             c->extra_text_groups[0] = 53;
             switch (button_id) {
                 case INFO_CULTURE:
-                    c->extra_text_ids[0] = (scenario_criteria_culture() <= 90)
+                    c->extra_text_ids[0] = (city_rating_culture() <= 90)
                         ? 9 + city_rating_explanation_for(SELECTED_RATING_CULTURE) : 50;
                     return 1;
-
                 case INFO_PROSPERITY:
-                    c->extra_text_ids[0] = (scenario_criteria_prosperity() <= 90)
+                    c->extra_text_ids[0] = (city_rating_prosperity() <= 90)
                         ? 16 + city_rating_explanation_for(SELECTED_RATING_PROSPERITY) : 51;
                     return 2;
                 case INFO_PEACE:
-                    c->extra_text_ids[0] = (scenario_criteria_peace() <= 90)
+                    c->extra_text_ids[0] = (city_rating_peace() <= 90)
                         ? 41 + city_rating_explanation_for(SELECTED_RATING_PEACE) : 52;
                     return 3;
                 case INFO_FAVOR:
-                    c->extra_text_ids[0] = (scenario_criteria_favor() <= 90)
+                    c->extra_text_ids[0] = (city_rating_favor() <= 90)
                         ? 27 + city_rating_explanation_for(SELECTED_RATING_FAVOR) : 53;
                     return 4;
                 case INFO_HEALTH:
