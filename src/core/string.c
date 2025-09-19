6314412c314890f8c0228fd037728ec66c389956
ZelionD <47186606+ZelionD@users.noreply.github.com>
2025-09-19 12:20:49 +0300

    refactor string_to_int and string_from_int (#1165)
    Co-authored-by: ZelionD

--- src/core/string.c
+++ src/core/string.c
@@ -82,34 +82,22 @@ const uint8_t *string_from_ascii(const char *str)
 
 int string_to_int(const uint8_t *str)
 {
-    static const int multipliers[] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
-    const uint8_t *ptr = str;
     int negative = 0;
-    int num_chars = 0;
-    if (*ptr == '-') {
+    if (*str == '-') {
         negative = 1;
-        ptr++;
-    }
-    while (*ptr >= '0' && *ptr <= '9') {
-        num_chars++;
-        ptr++;
+        ++str;
     }
 
-    if (num_chars > 8) {
-        return 0;
-    }
-    ptr = str;
-    if (*ptr == '-') {
-        ptr++;
-    }
     int result = 0;
-    while (num_chars) {
-        --num_chars;
-        result += multipliers[num_chars] * (*ptr - '0');
-        ptr++;
+    while (*str >= '0' && *str <= '9') {
+        if (result > 0) {
+            result *= 10;
+        }
+        result += *(str++) - '0';
     }
+
     if (negative) {
-        result = -result;
+        return -result;
     }
     return result;
 }
@@ -117,28 +105,39 @@ int string_to_int(const uint8_t *str)
 int string_from_int(uint8_t *dst, int value, int force_plus_sign)
 {
     int total_chars = 0;
-    if (value >= 0) {
-        if (force_plus_sign) {
-            dst[0] = '+';
-            dst++;
-            total_chars = 1;
-        }
-    } else {
-        dst[0] = '-';
-        dst++;
-        value = -value;
-        total_chars = 1;
-    }
-    int num_digits = calc_digits_in_number(value);
-    total_chars += num_digits;
 
-    dst[num_digits] = 0;
-    int digits_written = 0;
-    while (--num_digits >= 0) {
-        dst[num_digits] = (uint8_t) (value % 10 + '0');
-        value /= 10;
-        ++digits_written;
+    if (value < 0) {
+        dst[total_chars++] = '-';
+        value = -value;
+    } else if (force_plus_sign) {
+        dst[total_chars++] = '+';
     }
+
+    if (value == 0) {
+        dst[total_chars++] = '0';
+        dst[total_chars] = 0;
+        return total_chars;
+    }
+
+    uint8_t *nums = &dst[total_chars];
+    int total_nums = 0;
+
+    while (value > 0) {
+        nums[total_nums++] = (uint8_t) (value % 10 + '0');
+        value /= 10;
+        total_chars++;
+    }
+
+    // Reverse string
+    total_nums >>= 1;
+    while (total_nums > 0) {
+        char tmp = nums[total_nums - 1];
+        nums[total_nums - 1] = dst[total_chars - total_nums];
+        dst[total_chars - total_nums] = tmp;
+        total_nums--;
+    }
+
+    dst[total_chars] = 0;
 
     return total_chars;
 }
